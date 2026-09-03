52c5ee969c1a46e9b31849b3d020e94dc3ca45d2
atybus-create <arturtybus@o2.pl>
2026-09-03 12:14:26 +0200

    fix: retire legacy service worker without reload loop

--- sw.js
+++ sw.js
@@ -4,8 +4,6 @@ self.addEventListener('activate',event=>event.waitUntil((async()=>{
     const keys=await caches.keys();
     await Promise.all(keys.filter(k=>k.startsWith('mol-worker-app-')).map(k=>caches.delete(k)));
     await self.registration.unregister();
-    const clientsList=await self.clients.matchAll({type:'window',includeUncontrolled:true});
-    for(const client of clientsList) client.navigate(client.url);
   }catch(_){/* retirement best effort */}
 })()));
 self.addEventListener('fetch',()=>{});
