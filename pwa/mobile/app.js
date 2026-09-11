65aee7453daa1ae412b738a512e1bbd91e99e970
atybus-create <arturtybus@o2.pl>
2026-09-11 15:10:50 +0200

    MOL V3 Etap 4: bump mobile build to 20260911.9

--- pwa/mobile/app.js
+++ pwa/mobile/app.js
@@ -1,6 +1,6 @@
 (() => {
   'use strict';
-  const BUILD = '20260911.8';
+  const BUILD = '20260911.9';
   window.MOL_BUILD = BUILD;
   const shell = document.querySelector('.worker-shell');
   if (!shell) return;
