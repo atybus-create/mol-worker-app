4de75a918be7edb12659353dd92069cfbbdded28
atybus-create <arturtybus@o2.pl>
2026-09-11 15:40:19 +0200

    MOL V3 stage 4: force PWA refresh after UI fix

--- pwa/pwa-register.js
+++ pwa/pwa-register.js
@@ -5,7 +5,7 @@
   const script = document.currentScript;
   const scriptUrl = new URL(script.src, location.href);
   const scopeUrl = new URL("./", scriptUrl);
-  const workerUrl = new URL("./sw.js?v=20260911.5-stage3", scriptUrl);
+  const workerUrl = new URL("./sw.js?v=20260911.10-stage4", scriptUrl);
   let reloadAfterUpdate = false;
 
   function showUpdate(registration) {
