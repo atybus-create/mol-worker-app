1d6e0ca67b61d4d6f82af1f74e227fcb70e6611a
atybus-create <arturtybus@o2.pl>
2026-09-18 08:25:19 +0200

    Fix Android JVM target for APK build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,6 +15,15 @@ android {
         versionName = "3.0.0-internal.1"
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     buildTypes {
         debug {
             isMinifyEnabled = false
